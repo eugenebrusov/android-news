a5c4901361a022095d5a89a29fb1d7e6a47a3335
Eugene Brusov <eugenebrusov@gmail.com>
2017-08-28 16:41:00 +0200

    Adding Up navigation

--- News/app/src/main/java/com/eugenebrusov/news/NewsDetailActivity.kt
+++ News/app/src/main/java/com/eugenebrusov/news/NewsDetailActivity.kt
@@ -2,6 +2,7 @@ package com.eugenebrusov.news
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class NewsDetailActivity : AppCompatActivity() {
 
@@ -12,5 +13,8 @@ class NewsDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_detail)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
