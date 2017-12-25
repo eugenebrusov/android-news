fbc8a191ee3c9c27a6dac32cffd96c226ff566ca
Eugene Brusov <eugenebrusov@gmail.com>
2017-12-25 20:37:32 +0100

    Add setupViewModel() method to NewsListViewModelTest

--- News/app/src/test/java/com/eugenebrusov/news/newslist/NewsListViewModelTest.kt
+++ News/app/src/test/java/com/eugenebrusov/news/newslist/NewsListViewModelTest.kt
@@ -1,11 +1,28 @@
 package com.eugenebrusov.news.newslist
 
+import android.app.Application
+import com.eugenebrusov.news.data.source.Repository
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
 
 /**
  * Created by Eugene Brusov on 12/25/17.
  */
 class NewsListViewModelTest {
+
+    @Mock private lateinit var context: Application
+    @Mock private lateinit var repository: Repository
+    private lateinit var newsListViewModel: NewsListViewModel
+
+    @Before
+    fun setupViewModel() {
+        MockitoAnnotations.initMocks(this)
+
+        newsListViewModel = NewsListViewModel(context, repository)
+    }
+
     @Test
     fun loadNewsListFromRepositoryAndLoadIntoView() {
         newsListViewModel.loadNews()
