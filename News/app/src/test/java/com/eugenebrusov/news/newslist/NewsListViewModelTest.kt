1788ab15cec9308a7267ab3a6938850770766cf7
Eugene Brusov <eugenebrusov@gmail.com>
2017-12-25 17:38:27 +0100

    Add NewsListViewModelTest

---
+++ News/app/src/test/java/com/eugenebrusov/news/newslist/NewsListViewModelTest.kt
@@ -0,0 +1,13 @@
+package com.eugenebrusov.news.newslist
+
+import org.junit.Test
+
+/**
+ * Created by Eugene Brusov on 12/25/17.
+ */
+class NewsListViewModelTest {
+    @Test
+    fun loadNewsListFromRepositoryAndLoadIntoView() {
+
+    }
+}
