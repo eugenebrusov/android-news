21966ef5b5de5c188830200f62dc4baceb4c6554
Eugene Brusov <eugenebrusov@gmail.com>
2017-12-26 15:37:25 +0100

    Add test for getNews

--- News/app/src/test/java/com/eugenebrusov/news/newslist/NewsListViewModelTest.kt
+++ News/app/src/test/java/com/eugenebrusov/news/newslist/NewsListViewModelTest.kt
@@ -1,11 +1,17 @@
 package com.eugenebrusov.news.newslist
 
 import android.app.Application
+import com.eugenebrusov.news.data.source.DataSource
 import com.eugenebrusov.news.data.source.Repository
+import com.eugenebrusov.news.newslist.util.capture
 import org.junit.Before
 import org.junit.Test
+import org.mockito.ArgumentCaptor
+import org.mockito.ArgumentMatchers.eq
+import org.mockito.Captor
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.mockito.Mockito.verify
 
 /**
  * Created by Eugene Brusov on 12/25/17.
@@ -14,6 +20,7 @@ class NewsListViewModelTest {
 
     @Mock private lateinit var context: Application
     @Mock private lateinit var repository: Repository
+    @Captor private lateinit var loadNewsListCallbackCaptor: ArgumentCaptor<DataSource.LoadNewsListCallback>
     private lateinit var newsListViewModel: NewsListViewModel
 
     @Before
@@ -26,5 +33,7 @@ class NewsListViewModelTest {
     @Test
     fun loadNewsListFromRepositoryAndLoadIntoView() {
         newsListViewModel.loadNews()
+
+        verify(repository).getNews(eq(1), capture(loadNewsListCallbackCaptor))
     }
 }
