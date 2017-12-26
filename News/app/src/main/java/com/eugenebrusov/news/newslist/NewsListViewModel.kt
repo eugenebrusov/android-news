f2a63e146a8553a584155b94544b6386439b0ef5
Eugene Brusov <eugenebrusov@gmail.com>
2017-12-26 22:36:28 +0100

    Cleanup

--- News/app/src/main/java/com/eugenebrusov/news/newslist/NewsListViewModel.kt
+++ News/app/src/main/java/com/eugenebrusov/news/newslist/NewsListViewModel.kt
@@ -2,21 +2,13 @@ package com.eugenebrusov.news.newslist
 
 import android.app.Application
 import android.arch.lifecycle.AndroidViewModel
-import android.arch.lifecycle.MutableLiveData
 import android.databinding.ObservableArrayList
-import android.databinding.ObservableField
 import android.databinding.ObservableList
 import android.util.Log
 import com.eugenebrusov.news.SingleLiveEvent
-import com.eugenebrusov.news.api.NewsRetriever
 import com.eugenebrusov.news.data.source.DataSource
 import com.eugenebrusov.news.data.source.Repository
-import com.eugenebrusov.news.models.NewsListResponse
 import com.eugenebrusov.news.models.NewsResult
-import com.eugenebrusov.news.models.NewsResults
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 /**
  * Created by Eugene Brusov on 8/17/17.
