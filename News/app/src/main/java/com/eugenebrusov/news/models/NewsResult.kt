58d4835ee41a87ef4889230082abb225b4d875be
Eugene Brusov <eugenebrusov@gmail.com>
2018-01-07 13:22:12 +0100

    Clean NewsResult of Parcelable stuff

--- News/app/src/main/java/com/eugenebrusov/news/models/NewsResult.kt
+++ News/app/src/main/java/com/eugenebrusov/news/models/NewsResult.kt
@@ -10,35 +10,4 @@ import java.util.UUID
 data class NewsResult(val id: String?,
                       val webPublicationDate: String?,
                       val fields: NewsFields?,
-                      val tags: List<NewsTag>?) : Parcelable {
-
-    constructor() : this(UUID.randomUUID().toString(), null, null, null)
-
-    constructor(parcel: Parcel) : this(
-            parcel.readString(),
-            parcel.readString(),
-            parcel.readParcelable(NewsFields::class.java.classLoader),
-            parcel.createTypedArrayList(NewsTag)) {
-    }
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeString(id)
-        parcel.writeString(webPublicationDate)
-        parcel.writeParcelable(fields, flags)
-        parcel.writeTypedList(tags)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<NewsResult> {
-        override fun createFromParcel(parcel: Parcel): NewsResult {
-            return NewsResult(parcel)
-        }
-
-        override fun newArray(size: Int): Array<NewsResult?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+                      val tags: List<NewsTag>?)
