d834fdf0894f566412283113f23384af25fffdf6
Eugene Brusov <eugenebrusov@gmail.com>
2018-05-22 17:21:59 +0200

    Add loading state

--- News/app/src/main/java/com/eugenebrusov/news/newslist/NewsListPagedAdapter.kt
+++ News/app/src/main/java/com/eugenebrusov/news/newslist/NewsListPagedAdapter.kt
@@ -4,25 +4,42 @@ import android.arch.paging.PagedList
 import android.arch.paging.PagedListAdapter
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.eugenebrusov.news.R
 import com.eugenebrusov.news.data.model.NewsItem
 import com.eugenebrusov.news.data.model.Resource
 import com.eugenebrusov.news.data.model.Status
+import com.eugenebrusov.news.databinding.ItemNewsListLoadingStateBinding
 
 class NewsListPagedAdapter : PagedListAdapter<NewsItem, RecyclerView.ViewHolder>(ITEM_COMPARATOR) {
 
     var results: Resource<PagedList<NewsItem>>? = null
         set(value) {
+            val previousResults = field
+            val hadExtraRow = hasExtraRow()
+
             if (value?.status == Status.SUCCESS) {
-                super.submitList(value?.data)
+                super.submitList(value.data)
+            }
+
+            field = value
+            val hasExtraRow = hasExtraRow()
+            if (hadExtraRow != hasExtraRow) {
+                if (hadExtraRow) {
+                    notifyItemRemoved(super.getItemCount())
+                } else {
+                    notifyItemInserted(super.getItemCount())
+                }
+            } else if (hasExtraRow && previousResults?.status != value?.status) {
+                notifyItemChanged(itemCount - 1)
             }
         }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return when (viewType) {
             R.layout.item_news_list -> NewsListItemViewHolder.create(parent)
-            R.layout.item_news_list_network_state -> NewsListItemNetworkStateViewHolder.create(parent)
+            R.layout.item_news_list_loading_state -> NewsListItemLoadingStateViewHolder.create(parent)
             else -> throw IllegalArgumentException("unknown view type $viewType")
         }
     }
@@ -38,18 +55,18 @@ class NewsListPagedAdapter : PagedListAdapter<NewsItem, RecyclerView.ViewHolder>
     }
 
     override fun getItemViewType(position: Int): Int {
-//        return if (hasExtraRow() && position == itemCount - 1) {
-//            R.layout.item_news_list_network_state
-//        } else {
-            return R.layout.item_news_list
-//        }
+        return if (hasExtraRow() && position == itemCount - 1) {
+            R.layout.item_news_list_loading_state
+        } else {
+            R.layout.item_news_list
+        }
     }
 
-//    override fun getItemCount(): Int {
-//        return super.getItemCount() + if (hasExtraRow()) 1 else 0
-//    }
+    override fun getItemCount(): Int {
+        return super.getItemCount() + if (hasExtraRow()) 1 else 0
+    }
 
-//    private fun hasExtraRow() = networkState != null && networkState != NetworkState.LOADED
+    private fun hasExtraRow() = (Status.LOADING == results?.status || Status.ERROR == results?.status)
 
     companion object {
         val ITEM_COMPARATOR = object : DiffUtil.ItemCallback<NewsItem>() {
@@ -59,6 +76,19 @@ class NewsListPagedAdapter : PagedListAdapter<NewsItem, RecyclerView.ViewHolder>
 
             override fun areItemsTheSame(oldItem: NewsItem, newItem: NewsItem): Boolean =
                     oldItem.id == newItem.id
+        }
+    }
+
+    class NewsListItemLoadingStateViewHolder(
+            binding: ItemNewsListLoadingStateBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        companion object {
+            fun create(parent: ViewGroup?): NewsListItemLoadingStateViewHolder {
+                val binding = ItemNewsListLoadingStateBinding
+                        .inflate(LayoutInflater.from(parent?.context), parent, false)
+                return NewsListItemLoadingStateViewHolder(binding)
+            }
         }
     }
 }
